f5eb61e566d29f556fc0b91d541d60608f9d0003
cadu <boli69112@gmail.com>
2026-05-20 08:56:15 -0300

    feat: add a classe UsuarioService e o método registrar

---
+++ src/main/java/br/com/filmix/api/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package br.com.filmix.api.service;
+
+import br.com.filmix.api.dto.usuario.UsuarioRequestDTO;
+import br.com.filmix.api.dto.usuario.UsuarioResponseDTO;
+import br.com.filmix.api.exception.RegraDeNegocioException;
+import br.com.filmix.api.mapper.UsuarioMapper;
+import br.com.filmix.api.model.Usuario;
+import br.com.filmix.api.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioMapper usuarioMapper;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Transactional
+    public UsuarioResponseDTO registrar(UsuarioRequestDTO dto) {
+        if (usuarioRepository.findByEmail(dto.email()).isPresent()) {
+            throw new RegraDeNegocioException("O email informado já está em uso");
+        }
+
+        Usuario usuario = usuarioMapper.toEntity(dto);
+
+        String senhaCriptografada = passwordEncoder.encode(dto.senha());
+        usuario.setSenhaHash(senhaCriptografada);
+
+        usuarioRepository.save(usuario);
+        return usuarioMapper.toResponseDTO(usuario);
+    }
+
+}
