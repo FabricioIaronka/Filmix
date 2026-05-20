9682d6a9a304d4aefd858e4bbf58a19886f40259
cadu <boli69112@gmail.com>
2026-05-20 08:52:49 -0300

    feat: add mapper para relacionar a entidade usuario e os seus respectivos DTOs

---
+++ src/main/java/br/com/filmix/api/mapper/UsuarioMapper.java
@@ -0,0 +1,38 @@
+package br.com.filmix.api.mapper;
+
+import br.com.filmix.api.dto.usuario.UsuarioRequestDTO;
+import br.com.filmix.api.dto.usuario.UsuarioResponseDTO;
+import br.com.filmix.api.model.Role;
+import br.com.filmix.api.model.Usuario;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UsuarioMapper {
+
+
+    public Usuario toEntity(UsuarioRequestDTO dto) {
+        Usuario usuario = new Usuario();
+        usuario.setNome(dto.nome());
+        usuario.setEmail(dto.email());
+        usuario.setFotoPerfil(dto.fotoPerfil());
+
+        if (dto.role() == null) {
+            usuario.setRole(Role.USER);
+        } else {
+            usuario.setRole(dto.role());
+        }
+
+        return usuario;
+    }
+
+    public UsuarioResponseDTO toResponseDTO(Usuario entity) {
+        return new UsuarioResponseDTO(
+                entity.getId(),
+                entity.getNome(),
+                entity.getEmail(),
+                entity.getFotoPerfil(),
+                entity.getRole()
+        );
+    }
+
+}
