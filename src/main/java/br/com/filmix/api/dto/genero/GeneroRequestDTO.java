e9d5b5d9b2f2ea75cb4ff5a391169954ef2bff0a
Fabrício Souza <fabricioiaronka1@gmail.com>
2026-05-20 20:11:38 -0300

    feat: Adicionando DTO's de Genero

---
+++ src/main/java/br/com/filmix/api/dto/genero/GeneroRequestDTO.java
@@ -0,0 +1,14 @@
+package br.com.filmix.api.dto.genero;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record GeneroRequestDTO(
+        @NotBlank(message = "O nome não pode ser vazio")
+        @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+        String nome,
+
+        String cor,
+        String icone
+) {
+}
