917f9f00fc97f1951131f180a37dc5053814409d
cadu <boli69112@gmail.com>
2026-05-20 08:21:28 -0300

    fix: corrigindo as classes Usuario e UsuarioFilme

--- src/main/java/br/com/filmix/api/model/Usuario.java
+++ src/main/java/br/com/filmix/api/model/Usuario.java
@@ -1,21 +1,90 @@
 package br.com.filmix.api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 @Entity
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
 
+    @Column(name = "email", length = 200, unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "senha_hash", length = 255, nullable = false)
+    private String senhaHash;
+
+    @Column(name = "foto_perfil", length = 255)
+    private String fotoPerfil;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Avaliacao> avaliacoes;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UsuarioFilme> listaDeFilmes;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if (this.role == Role.ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        }
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senhaHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
--- src/main/java/br/com/filmix/api/model/UsuarioFilme.java
+++ src/main/java/br/com/filmix/api/model/UsuarioFilme.java
@@ -19,74 +19,21 @@ import java.util.Set;
 @AllArgsConstructor
 @Entity
 @Table(name = "usuario_filme")
-public class UsuarioFilme implements UserDetails {
+public class UsuarioFilme {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome", length = 100, nullable = false)
-    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
-    @Column(name = "email", length = 200, unique = true, nullable = false)
-    private String email;
+    @ManyToOne
+    @JoinColumn(name = "filme_id")
+    private Filme filme;
 
-    @Column(name = "senha_hash", length = 255, nullable = false)
-    private String senhaHash;
-
-    @Column(name = "foto_perfil", length = 255)
-    private String fotoPerfil;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private Role role = Role.USER;
-
-    @OneToMany(mappedBy = "usuario")
-    private List<Avaliacao> avaliacoes;
-
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<UsuarioFilme> listaDeFilmes;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-
-        if (this.role == Role.ADMIN) {
-            return List.of(
-                    new SimpleGrantedAuthority("ROLE_ADMIN"),
-                    new SimpleGrantedAuthority("ROLE_USER")
-            );
-        }
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.senhaHash;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+    @Column(name = "visto", nullable = false)
+    private boolean visto = false;
 
 }
